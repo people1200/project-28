4f3230e593cd3574f0060aebc9529bdb80b8445a
people1200 <78105418+people1200@users.noreply.github.com>
2021-05-05 20:34:27 +0100

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,34 @@
+class launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.004,
+            length: 10
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+      
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+    attach(body){
+        this.sling.bodyA=body
+    }
+
+    display(){
+    
+
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+           
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            
+    }
+    
+}
+}
